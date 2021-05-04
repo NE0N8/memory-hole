e51ca218ce7e0f13cfd2c5c9870c268d0e5cc9f3
NE0N8 <gnosis@xmail.net>
2021-05-04 13:15:18 -0400

    Index page for restricting access to directory

---
+++ thumbs/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" style="background: url('../background.png') 100% 100%; overflow: hidden;">
+
+<head>
+   <title> P R I V A T E </title>
+   <link rel="icon" href="../sol.png">
+   <link rel="stylesheet" href="../home.css" type="text/css">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta charset="UTF-8">
+</head>
+<body style="overflow: hidden; box-shadow: 0 0 25vw black inset, 0 0 25vw black inset !important;">
+      <div id="backGif"></div>
+      <a class="center" href="../index.php">R E T U R N</a>
+</body>
+</html>
